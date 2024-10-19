cb7469fc87d1f3ae99ee14ed717e9c9318f78b72
Kolluru Sumanth <kollurusumanth004@gmail.com>
2024-10-19 19:57:21 +0530

    fixed some issues

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
 import { assets } from '../../assets/frontend_assets/assets';
 import './navbar.css';
-
+import { Link } from 'react-router-dom';
 const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("Home");
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="Grab Hub" className='logo' />
+      <Link to="/"><img src={assets.logo} alt="Grab Hub" className='logo' /></Link>
       <ul className="navbar-menu">
-        {["Home", "Menu","Mobile-App", "Contact-Us"].map((item) => (
-          <li
-            key={item}
-            onClick={() => setMenu(item)}
-            className={menu === item ? "active" : ""}
-          >
-            {item}
-          </li>
-        ))}
+        <Link to={"/"} onClick={()=>{setMenu("Home")}} className={menu==="Home"? "active": ""}>Home</Link>
+        <a href='#explore-menu' onClick={()=>{setMenu("Menu")}} className={menu==="Menu"? "active": ""}>Menu</a>
+        <a href='#app-download' onClick={()=>{setMenu("Mobile-App")}} className={menu==="Mobile-App"? "active": ""}>Moblie-App</a>
+        <a href='#footer' onClick={()=>{setMenu("Contact-Us")}} className={menu==="Contact-Us"? "active": ""}>Contact-Us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search" />
